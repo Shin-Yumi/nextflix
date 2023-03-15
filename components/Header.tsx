e678361961b3cf0cb58e79f4ae1025f7b73fef9e
Shin-Yumi <tlsdbal0517@naver.com>
2023-03-15 09:39:11 +0900

    init

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+	return (
+		<header>
+			<h1>nextflix</h1>
+
+			<ul>
+				<li>menu</li>
+				<li>menu</li>
+				<li>menu</li>
+				<li>menu</li>
+				<li>menu</li>
+			</ul>
+		</header>
+	);
+}
+
+export default Header;
