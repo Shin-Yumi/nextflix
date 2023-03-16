7fb4cd50331d7859b4ca7d365dbddc3655376a16
Shin-Yumi <tlsdbal0517@naver.com>
2023-03-16 23:23:10 +0900

    api 설정 오류 해결

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Header from '@/components/Header';
+import requests from '@/utils/requests';
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
+	console.log(props);
 	return (
 		<div className='relative h-screen bg-gradient-to-b from-[#333] to-[#141414]'>
 			<Head>
@@ -20,3 +22,27 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+	const [top, sf, drama, fantasy, thriller, animation, anime] = await Promise.all([
+		fetch(requests.top).then((res) => res.json()),
+		fetch(requests.sf).then((res) => res.json()),
+		fetch(requests.drama).then((res) => res.json()),
+		fetch(requests.fantasy).then((res) => res.json()),
+		fetch(requests.thriller).then((res) => res.json()),
+		fetch(requests.animation).then((res) => res.json()),
+		fetch(requests.anime).then((res) => res.json()),
+	]);
+
+	return {
+		props: {
+			anime: anime.results,
+			topRated: top.results,
+			sf: sf.results,
+			drama: drama.results,
+			fantasy: fantasy.results,
+			thriller: thriller.results,
+			animation: animation.results,
+		},
+	};
+};
