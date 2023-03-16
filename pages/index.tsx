18743731d71ae87b4a17778e3c50ccf568ac4808
Shin-Yumi <tlsdbal0517@naver.com>
2023-03-16 23:50:31 +0900

    fetching한 데이터들, 인덱스 props 인터페이스 타입 지정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,48 @@
+import Header from '@/components/Header';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Header from '@/components/Header';
 import requests from '@/utils/requests';
+import { Movie } from '@/typings';
 
-const Home: NextPage = (props) => {
-	console.log(props);
+interface IndexProps {
+	topRated: Movie[];
+	sf: Movie[];
+	drama: Movie[];
+	fantasy: Movie[];
+	thriller: Movie[];
+	animation: Movie[];
+}
+
+const Home: NextPage<IndexProps> = ({ topRated, sf, drama, fantasy, thriller, animation }: IndexProps) => {
+	console.log(sf);
+
 	return (
 		<div className='relative h-screen bg-gradient-to-b from-[#333] to-[#141414]'>
 			<Head>
-				<title>Nextflix</title>
+				<title>Netflix clon-coding</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-
 			<Header />
-
 			<main className=''>
 				<section></section>
 			</main>
 		</div>
 	);
 };
-
 export default Home;
 
 export const getServerSideProps = async () => {
-	const [top, sf, drama, fantasy, thriller, animation, anime] = await Promise.all([
+	const [top, sf, drama, fantasy, thriller, animation] = await Promise.all([
 		fetch(requests.top).then((res) => res.json()),
 		fetch(requests.sf).then((res) => res.json()),
 		fetch(requests.drama).then((res) => res.json()),
 		fetch(requests.fantasy).then((res) => res.json()),
 		fetch(requests.thriller).then((res) => res.json()),
 		fetch(requests.animation).then((res) => res.json()),
-		fetch(requests.anime).then((res) => res.json()),
 	]);
 
 	return {
 		props: {
-			anime: anime.results,
 			topRated: top.results,
 			sf: sf.results,
 			drama: drama.results,
